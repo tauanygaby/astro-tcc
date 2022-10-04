bed741d5cb2d66fcc344a1e6697620bb3c5f09c8
TAUANY GABRIELLY DALTRO FIGUEIREDO <tauany.figueiredo@bradesco.com.br>
2022-10-03 22:42:45 -0300

    Criacao edicao de texto

---
+++ astro/src/app/editor/editor.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css']
+})
+export class EditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+   
+  };
+
+}
