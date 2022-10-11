146e3fb8a71564c6121ecad0d14b58d3c519ef40
alinetr-cit <alinetr@ciandt.com>
2022-10-10 22:03:36 -0300

    tela de checklist - banco de dados

--- astro/src/app/modules/checklist/checklist.component.ts
+++ astro/src/app/modules/checklist/checklist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CrudService } from 'src/app/service/crud.service';
+import { ChecklistModel } from '../checklist.model';
 
 @Component({
   selector: 'app-checklist',
@@ -8,7 +10,21 @@ import { Router } from '@angular/router';
 })
 export class ChecklistComponent implements OnInit {
 
+  checklist : ChecklistModel = new ChecklistModel();
+
+  constructor(private crudService: CrudService, private router: Router) { }
+
   ngOnInit() {
+  }
+  
+  cadastrar() {
+    console.log(this.checklist);
+    this.crudService.cadastrarChecklist(this.checklist).subscribe(checklist => {
+      this.checklist = new ChecklistModel();
+      
+    }, err => {
+      (console.log("Erro ao cadastrar banana", err));
+    })
   }
 
 }
