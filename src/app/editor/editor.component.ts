88eff037ac5eec67338b03fc6c845c64ac822d09
TAUANY GABRIELLY DALTRO FIGUEIREDO <tauany.figueiredo@bradesco.com.br>
2022-11-07 22:46:36 -0300

    Testando export pdf

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { jsPDF } from "jspdf";
 import { EditorModel } from '../modules/editor.model';
 import { CrudService } from '../service/crud.service';
 
@@ -9,10 +10,17 @@ import { CrudService } from '../service/crud.service';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  @ViewChild('content', { static: true }) content: ElementRef;  
 
   editor: EditorModel = new EditorModel();
 
-  public Editor = ClassicEditor;
+  Data = [  
+    { Id: 101, Name: 'Nitin', Salary: 1234 },  
+    { Id: 102, Name: 'Sonu', Salary: 1234 },  
+    { Id: 103, Name: 'Mohit', Salary: 1234 },  
+    { Id: 104, Name: 'Rahul', Salary: 1234 },  
+    { Id: 105, Name: 'Kunal', Salary: 1234 }  
+  ];  
 
   constructor(private crudService: CrudService) { }
 
@@ -28,18 +36,39 @@ export class EditorComponent implements OnInit {
     })
   }
 
-  // config: AngularEditorConfig = {
-  //   editable: true,
-  //   spellcheck: true,
-  //   outline: true,
-  //   width: '43.5rem',
-  //   height: '30rem',
-  //   minHeight: '5rem',
-  //   placeholder: 'Enter text here...',
-  //   translate: 'no',
-  //   defaultParagraphSeparator: 'p',
-  //   defaultFontName: 'Arial',
-  //   defaultFontSize: '4',
-  // };
+  public SavePDF(): void {  
+    let content=this.content.nativeElement;  
+    let doc = new jsPDF();  
+    let _elementHandlers =  
+    {  
+      '#editor':function(element,renderer){  
+        return true;  
+      }  
+    };  
+    doc.html(content.innerHTML),{  
+  
+      'width':190,  
+      'elementHandlers':_elementHandlers  
+    };  
+
+    console.log(doc.html);
+    
+  
+    doc.save('test.pdf');  
+  }   
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    outline: true,
+    width: '43.5rem',
+    height: '30rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    defaultFontSize: '4',
+  };
 
 }
