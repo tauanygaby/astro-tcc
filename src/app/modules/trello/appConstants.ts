1b5c9369d4496bee0a9ced22ad49ac54699f9bd5
alinetr-cit <alinetr@ciandt.com>
2022-11-10 00:35:47 -0300

    traduzindo trello e retirando campo de tag

--- src/app/modules/trello/appConstants.ts
+++ src/app/modules/trello/appConstants.ts
@@ -4,25 +4,29 @@ export const appConstants = {
 
     /** Issue Types with ttheir hardcoded colors */
     issueTypeListWithColor: {
+
+        [IssueType.Task]: {
+            name: IssueType.Task,
+            color: '#99ccff61'
+        },
+
+        [IssueType.SubTask]: {
+            name: IssueType.SubTask,
+            color: '#3d7e9a4d'
+        },
+
         [IssueType.Bug]: {
             name: IssueType.Bug,
             color: '#99333352'
-        },
-        [IssueType.Epic]: {
-            name: IssueType.Epic,
-            color: '#33996652'
         },
         [IssueType.Story]: {
             name: IssueType.Story,
             color: '#fff3d4'
         },
-        [IssueType.Task]: {
-            name: IssueType.Task,
-            color: '#99ccff61'
-        },
-        [IssueType.SubTask]: {
-            name: IssueType.SubTask,
-            color: '#3d7e9a4d'
+
+        [IssueType.Epic]: {
+            name: IssueType.Epic,
+            color: '#33996652'
         }
     }
 };
